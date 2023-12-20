68b78160b39428fa17934b7b3aab68f80d777379
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2023-12-19 22:04:56 -0400

    Agregadas funcion para tomar entradas y guardarlas en un array, funciones para borrar un elemento y todos de la pantalla

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const dc = document.querySelector();
-
-const c = dc('#c');
-
-console.log(c.textContent);
+const equacion = [];
+function borrarUno() {
+    let borrado = equacion.pop();
+    console.log(equacion)
+}
+function borrarTodo() {
+    for(let i = equacion.length; i > 0;i--) {
+        let el = equacion.pop();
+    }
+    console.log(equacion)
+}
+function getEntries(entrie) {
+    equacion.push(entrie);
+    console.log(typeof entrie);
+    console.log(equacion)
+}
