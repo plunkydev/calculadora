7ea5b924acec4711387b63fc35141d31ea3bec4d
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2023-12-22 19:49:49 -0400

    Terminado codigo calculadora

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 const equacion = [];
+const pantalla = document.querySelector('#pantalla');
+let numeros = [];
+let letras = '';
 function borrarUno() {
     let borrado = equacion.pop();
-    console.log(equacion)
+    letras = equacion.join('');
+    pantalla.innerText = letras;
+    if (letras.length === 0) {
+        pantalla.innerText = '000000000';
+    }
 }
 function borrarTodo() {
     for(let i = equacion.length; i > 0;i--) {
         let el = equacion.pop();
+        pantalla.innerText = '000000000';
     }
-    console.log(equacion)
 }
 function getEntries(entrie) {
-    equacion.push(entrie);
-    console.log(typeof entrie);
-    console.log(equacion)
+        equacion.push(entrie);
+        letras = equacion.join('');
+        pantalla.innerText = letras;
+}
+function realizarOperacion(operacion) {
+let numeros = operacion.match(/\d+(\.\d+)?/g);
+let operadores = operacion.match(/[\%\+\-\*\/]/g);
+let resultado = parseFloat(numeros[0]);
+for (let i = 0; i < operadores.length; i++) {
+let num = parseFloat(numeros[i + 1]);
+let operador = operadores[i];
+if (operador === "+") {
+    resultado += num;
+} else if (operador === "-") {
+    resultado -= num;
+} else if (operador === "*") {
+    resultado *= num;
+} else if (operador === "/") {
+    resultado /= num;
+} else if (operador === "%") {
+    resultado **= num;
+}
+}
+return resultado;
+}
+
+function total() {
+    pantalla.innerText = realizarOperacion(letras);
 }
